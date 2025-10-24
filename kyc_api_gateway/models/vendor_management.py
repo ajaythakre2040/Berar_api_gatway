f3c73ee806c7ac46fc23aebf21c3c4f2eccdb7ae
athargithubcoding <junaidathar7>
2025-10-24 12:23:37 +0530

    vendor required filed

--- kyc_api_gateway/models/vendor_management.py
+++ kyc_api_gateway/models/vendor_management.py
@@ -8,10 +8,10 @@ class VendorManagement(models.Model):
     header_key_name = models.CharField(max_length=255, unique=True)
     uat_base_url = models.CharField(max_length=255, unique=True)
     uat_api_key = models.CharField(max_length=255, unique=True)
-    prod_base_url = models.CharField(max_length=255, unique=True)
-    prod_api_key = models.CharField(max_length=255, unique=True)
-    contact_phone = models.CharField(max_length=10, blank=True, null=True)
-    contact_email = models.EmailField(max_length=255, unique=True)
+    prod_base_url = models.CharField(max_length=255,unique=True,blank=True, null=True)
+    prod_api_key = models.CharField(max_length=255,unique=True,blank=True, null=True)
+    contact_phone = models.CharField(max_length=10,blank=True, null=True,unique=True)
+    contact_email = models.EmailField(max_length=255,unique=True,blank=True, null=True)
     status = models.BooleanField(default=False)
 
     created_by = models.IntegerField()
@@ -19,7 +19,7 @@ class VendorManagement(models.Model):
     updated_by = models.IntegerField(null=True, blank=True)
     updated_at = models.DateTimeField(null=True, blank=True)
     deleted_by = models.IntegerField(null=True, blank=True)
-    deleted_at = models.DateTimeField(null=True, blank=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)git
 
     class Meta:
         db_table = "kyc_vendor_management"
